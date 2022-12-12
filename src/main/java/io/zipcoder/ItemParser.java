a03e1110cba74aad15f767768a38bad2d1428948
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-12-12 16:01:51 -0500

    passes broken single item

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,6 +2,7 @@ package io.zipcoder;
 
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
+import io.zipcoder.utils.match.MatchGroup;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,46 +16,89 @@ public class ItemParser {
         String[] items = valueToParse.split("##");
         System.out.println("items arr: " + Arrays.toString(items));
         for (String s : items) {
-            Item temp = null;
             try {
-                temp = parseSingleItem(s);
+                list.add(parseSingleItem(s));
             } catch (ItemParseException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println("item being added to list: " + temp);
-            list.add(temp);
         }
         System.out.println("list: " + list);
         return list;
-//        return null;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        String[] keyValuePairs = singleItem.split(";");
-        String regexStr = "[/W]";
-        Pattern pattern = Pattern.compile(regexStr);
-        Matcher matcher = pattern.matcher(singleItem);
-        System.out.println("key value pairs arr: " + Arrays.toString(keyValuePairs));
-        String[] temp = new String[4];
-        int tempIndex = 0;
-//        while (matcher.find()) {
-        for (String s : keyValuePairs) {
-            String[] keyValueSplit = s.split("[:@^%*]");
-            keyValueSplit[1] = keyValueSplit[1].toLowerCase();
-            System.out.println("key value split index[1]: " + keyValueSplit[1]);
-            temp[tempIndex] = keyValueSplit[1].replaceAll("#", "");
-            System.out.println("temp index: " + tempIndex);
-            tempIndex++;
-            System.out.println("-------------------------");
-        }
+        //this one doesn't work
+//        String[] keyValuePairs = singleItem.split(";");
+////        Pattern pattern = Pattern.compile(regexStr);
+////        Matcher matcher = pattern.matcher(singleItem);
+//        String[] temp = new String[4];
+//        int tempIndex = 0;
+////        while (matcher.find()) {
+//        for (String s : keyValuePairs) {
+//            Pattern pattern = Pattern.compile("^[:@^%*]");
+//            Matcher matcher = pattern.matcher(singleItem);
+//            Boolean match = matcher.matches();
+//            while (matcher.find()) {
+//                if (match) {
+//                    temp[tempIndex] = matcher.toString();
+//                    System.out.println("tempArr of just after : part: " + Arrays.toString(temp));
+////                String[] keyValueSplit = s.split("[:@^%*]");
+////                keyValueSplit[1] = keyValueSplit[1].toLowerCase();
+////                temp[tempIndex] = keyValueSplit[1].replaceAll("#", "");
+//                    tempIndex++;
+//                }
+//            }
 //        }
-        String name = temp[0];
-        Double price = Double.valueOf(temp[1]);
-        String type = temp[2];
-        String expiration = temp[3];
-        Item item = new Item(name, price, type, expiration);
-        System.out.println(item);
-        return item;
-//        return null;
+////        }
+//        String name = temp[0];
+//        Double price = Double.valueOf(temp[1]);
+//        String type = temp[2];
+//        String expiration = temp[3];
+//        Item item = new Item(name, price, type, expiration);
+//        System.out.println(item);
+//        return item;
+
+
+        //works
+        String[] keyValuePairs = singleItem.split(";");
+//        if (!(keyValuePairs.length == 4)) {
+//            throw new ItemParseException();
+//        }
+
+//        for (String s : keyValuePairs)
+//                if (!s.contains(":")) {
+//
+//            }
+//        }
+//        String regexStr = "[:@^%*]";
+//        Pattern pattern = Pattern.compile(regexStr);
+//        Matcher matcher = pattern.matcher(singleItem);
+            System.out.println("key value pairs arr: " + Arrays.toString(keyValuePairs));
+            String[] temp = new String[4];
+            int tempIndex = 0;
+//        while (matcher.find()) {
+            for (String s : keyValuePairs) {
+
+                String[] keyValueSplit = s.split("[:@^%*]");
+                if (keyValueSplit.length == 1) {
+                    throw new ItemParseException();
+                } else {
+
+                    keyValueSplit[1] = keyValueSplit[1].toLowerCase();
+                    System.out.println("key value split index[1]: " + keyValueSplit[1]);
+                    temp[tempIndex] = keyValueSplit[1].replaceAll("#", "");
+                    System.out.println("temp index: " + tempIndex);
+                    tempIndex++;
+                    System.out.println("-------------------------");
+                }
+            }
+//        }
+            String name = temp[0];
+            Double price = Double.valueOf(temp[1]);
+            String type = temp[2];
+            String expiration = temp[3];
+            Item item = new Item(name, price, type, expiration);
+            System.out.println(item);
+            return item;
+        }
     }
-}
