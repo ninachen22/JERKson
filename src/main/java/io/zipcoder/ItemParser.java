a1a5cc7605fe9b6e7cdb4531ffc68afbb1d7f70f
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-12-12 13:46:33 -0500

    parse item list, missing pass on one test

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,6 +3,7 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -10,34 +11,32 @@ import java.util.regex.Pattern;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
-        return null;
-//        List<Item> list = new ArrayList<>();
-//        String[] items = valueToParse.split("##");
-//        System.out.println(Arrays.toString(items));
-//
-//        for (String item : items) {
-//            list.add(parseSingleItem(item));
-//        }
-//        return list;
+        List<Item> list = new ArrayList<>();
+        String[] items = valueToParse.split("##");
+        System.out.println("items arr: " + Arrays.toString(items));
+        for (String s : items) {
+            Item temp = null;
+            try {
+                temp = parseSingleItem(s);
+            } catch (ItemParseException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("item being added to list: " + temp);
+            list.add(temp);
+        }
+        System.out.println("list: " + list);
+        return list;
+//        return null;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        String name = "";
-        Double price = 0.00;
-        String type = "";
-        String expiration = "";
-//        Item item = new Item(name, price, type, expiration);
-
         String[] keyValuePairs = singleItem.split(";");
         String regexStr = "[/W]";
         Pattern pattern = Pattern.compile(regexStr);
         Matcher matcher = pattern.matcher(singleItem);
-
         System.out.println("key value pairs arr: " + Arrays.toString(keyValuePairs));
-
         String[] temp = new String[4];
         int tempIndex = 0;
-
 //        while (matcher.find()) {
         for (String s : keyValuePairs) {
             String[] keyValueSplit = s.split("[:@^%]");
@@ -49,14 +48,13 @@ public class ItemParser {
             System.out.println("-------------------------");
         }
 //        }
-        name = temp[0];
-        price = Double.valueOf(temp[1]);
-        type = temp[2];
-        expiration = temp[3];
+        String name = temp[0];
+        Double price = Double.valueOf(temp[1]);
+        String type = temp[2];
+        String expiration = temp[3];
         Item item = new Item(name, price, type, expiration);
         System.out.println(item);
         return item;
-
 //        return null;
     }
 }
